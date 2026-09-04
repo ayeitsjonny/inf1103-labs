767a420ab3f124f940c422bdbe9b19d7916b4352
Jonathan New <2603043@sit.singaporetech.edu.sg>
2026-09-04 15:11:31 +0800

    Added Profile Variables

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -19,3 +19,13 @@ print("Day 3:", followers)
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+username = input("Enter username: ") # Activity 4
+age = int(input("Enter age: "))
+category = input("Enter content category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Content Category:", category)
