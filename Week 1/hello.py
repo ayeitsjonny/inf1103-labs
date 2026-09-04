530afc75c0a9c943af761587422566bdd28c918f
Jonathan New <2603043@sit.singaporetech.edu.sg>
2026-09-04 15:08:49 +0800

    Dynamic Profile

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -3,3 +3,10 @@ print("Welcome here")
 print("My first post!")
 print("====================")
 
+username = "jonnyboy" # Activity 2
+bio = "I am a software developer and I love coding."
+followers = 150
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
