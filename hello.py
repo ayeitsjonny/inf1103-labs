dfaa743035a4ecc7a789cab78216cadb3db7042f
ayeitsjonny <newkahhweejonathan@gmail.com>
2026-09-04 14:23:23 +0800

    Another update

--- hello.py
+++ hello.py
@@ -1,21 +1,9 @@
-print("=================")
-print("Welcome here")
-print("My first post!")
-print("=================")
+username = input("jonnyboy")
+age = input("22")
+category = input("Technology")
 
-username = "jonnyboy"
-bio = "I love coding and sharing my projects with the world."
-followers = 150
-
-followers += 50  # Simulating gaining new followers
-print("Day 1:", followers)
-
-followers += 20  # Simulating gaining new followers
-print("Day 2:", followers)
-
-followers += 10  # Simulating gaining new followers
-print("Day 3:", followers)
-
+print("\nInstagram Profile")
+print("==================")
 print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+print("Age:", age)
+print("Category:", category)
